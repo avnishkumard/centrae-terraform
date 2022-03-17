00583276ae55a03bae6339cd01653b950200a3e9
leon.nunes <leon.nunes@signiance.com>
2022-03-17 13:01:24 +0530

    Resolved s3 conflict due to acl=private
    Signed-off-by: leon.nunes <leon.nunes@signiance.com>

--- s3.tf
+++ s3.tf
@@ -1,6 +1,5 @@
 resource "aws_s3_bucket" "bucket" {
   count         = "${length(var.s3_bucket_name)}"
   bucket        = "${var.s3_bucket_name[count.index]}"
-  acl           = "private"
   force_destroy = "true"
 }
