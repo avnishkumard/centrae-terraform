ed5ff4864633ba835c7b59a365dc18f992d63a91
avnish858 <Avnishkumardwivedi858@gmail.com>
2022-03-24 00:04:10 +0530

    added db instance

---
+++ db_instance/terraform.tfvars
@@ -0,0 +1,10 @@
+engine = "mysql"
+engine_varsion ="8.0.27"
+db_family ="mysql8.0"
+db_major_engine_version ="8.0"
+instance_type ="db.t3.micro"
+allocated_storage =20
+max_allocated_storage =100
+db_name ="avnish"
+username ="avnish1"
+performance_insights_retention_period=7
---
+++ db_instance/variables.tf
@@ -0,0 +1,10 @@
+variable "engine" {}
+variable "engine_varsion" {}
+variable "db_family" {}
+variable "db_major_engine_version" {}
+variable "instance_type" {}
+variable "allocated_storage" {}
+variable "max_allocated_storage" {}
+variable "db_name" {}
+variable "username" {}
+variable "performance_insights_retention_period" {}
