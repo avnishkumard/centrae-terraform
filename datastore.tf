0a4eb632ed8a9af0facf4739cfe4b25fbbe3b1ba
avnishkumard <113968698+avnishkumard@users.noreply.github.com>
2023-08-08 01:19:34 +0530

    Update datastore.tf

--- datastore.tf
+++ datastore.tf
@@ -7,7 +7,7 @@ output "account_id" {
 output "caller_arn" {
   value = data.aws_caller_identity.current.arn
 }
-
+afdadfadfa
 output "caller_user" {
   value = data.aws_caller_identity.current.user_id
 }
